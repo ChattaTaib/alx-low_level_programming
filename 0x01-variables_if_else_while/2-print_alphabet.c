80241a824ba5397b3761359e0f18b0555ff42c54
Chatta Taib <suppertcc@gmail.com>
2022-09-09 13:07:06 -0700

    If Else While

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	       putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
