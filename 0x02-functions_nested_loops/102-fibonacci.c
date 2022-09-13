93a85a500d0d6dc79882223dfdccba0d8040672a
Chatta Taib <suppertcc@gmail.com>
2022-09-13 15:13:22 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int count = 2;
+	long int x = 1, y = 2;
+	long int z;
+
+	printf("%lu, ", x);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", y);
+		}
+		else
+		{
+			printf("%lu, ", y);
+		}
+
+		z = y;
+		y += x;
+		x = z;
+		count++;
+	}
+
+	return (0);
+}
